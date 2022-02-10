83a100cc9fd5491ce77afc5df535de790b4ef96a
reginabecker <thinaregina@gmail.com>
2022-02-10 11:56:40 -0300

    Update crud.js

--- api/crud.js
+++ api/crud.js
@@ -2,16 +2,13 @@ import Task from './models/task';
 
 import { publishToQueue } from './mqservice';
 
-<<<<<<< HEAD
 const defaultQueue = "tasks";
-=======
 var defaultQueue;
 
 if (process.env.NODE_ENV == 'dev')
     defaultQueue = 'tasksdev'
 else
     defaultQueue = 'tasks'
->>>>>>> a6c551d (primeiro commit)
 
 export default {
     create: (req, res) => {
@@ -21,11 +18,8 @@ export default {
         task.save((err, data) => {
             if (!err) {
                 // console.log(msg)
-<<<<<<< HEAD
                 let msg = { html: `<h1>Ninja Tasks:</h1><p>Tarefa ${task.title} criada com sucesso!</p>`, email: task.owner }
-=======
                 let msg = { html: `<h1>Treko:</h1><p>Tarefa ${task.title} criada com sucesso!</p>`, email: task.owner }
->>>>>>> a6c551d (primeiro commit)
                 publishToQueue(defaultQueue, JSON.stringify(msg));
                 return res.status(200).json({ data: data })
             }
@@ -34,26 +28,20 @@ export default {
                 return res.status(400).json(err)
             }
 
-<<<<<<< HEAD
-=======
-            if (err.name === "MongoError") {
+           if (err.name === "MongoError") {
                 return res.status(409).json(err)
             }
 
->>>>>>> a6c551d (primeiro commit)
             return res.status(500).json(err)
         })
     },
     list: (req, res) => {
         let query = {}
 
-<<<<<<< HEAD
         if (req.query.name) {
             query.name = new RegExp(req.query.name, 'i')
-=======
         if (req.query.title) {
             query.title = new RegExp(req.query.title, 'i')
->>>>>>> a6c551d (primeiro commit)
         }
 
         Task.find(query, {}, { sort: '-date' }, (err, result) => {
